28dde9831c8f622c47ecf6496982666732a21fdc
ortizProg <santiagoortiz012@gmail.com>
2026-03-19 01:18:53 -0500

    add port in url servicios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     appVersion: 'v1.0.0',
-    urlServicios: 'http://159.89.149.223',
+    urlServicios: 'http://159.89.149.223:3000',
     logoutTimeout: 900000,
     environment: 'production',
 }
